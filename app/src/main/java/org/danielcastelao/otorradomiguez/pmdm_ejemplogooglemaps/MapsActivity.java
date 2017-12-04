466a216472e9d499838c0b5bd7ea671daf4d22bf
omartorrado <frikimoro@gmail.com>
2017-12-04 11:00:45 +0100

    Iniciando proyecto

---
+++ app/src/main/java/org/danielcastelao/otorradomiguez/pmdm_ejemplogooglemaps/MapsActivity.java
@@ -0,0 +1,56 @@
+package org.danielcastelao.otorradomiguez.pmdm_ejemplogooglemaps;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps_b);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        /* Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        */
+
+        //Guardamos coordenadas en una variable
+        LatLng danielCastelao=new LatLng(42.236574, -8.714311);
+
+        //Creamos un marcador en las coordenadas anteriores
+        mMap.addMarker(new MarkerOptions().position(danielCastelao).title("CFP Daniel Castelao"));
+
+        //Movermos la camara a esa posicion
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(danielCastelao,15));
+    }
+}
